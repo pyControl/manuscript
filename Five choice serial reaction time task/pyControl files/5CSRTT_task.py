a083fb04d01ce514ecc01c703206ce920799f92f
takam <thomas.akam@psy.ox.ac.uk>
2021-02-26 11:32:02 +0000

    - Replaced individual task file for different 5CSRT training stages and challanges with a single file that can be used for all conditions by setting variables.

--- Five choice serial reaction time task/pyControl files/5CSRTT_task.py
+++ Five choice serial reaction time task/pyControl files/5CSRTT_task.py
@@ -1,29 +1,42 @@
-#--------------------------5-CSRTT Variable iti Protocol Task File----------------------
+#--------------------------5-CSRTT Protocol----------------------
 
-# cue presentation ==> Correct response       ==> reward(@reward port)      ==> iti(long)** ==> cue presentation
-# cue presentation ==> Incorrect response     ==> penalty (house light off) ==> iti(long)** ==> cue presentation
-# cue presentation ==> omission (no response) ==> penalty (house light off) ==> iti(long)** ==> cue presentation
-# **any response at 5-Poke during the iti is considered as premature response and trial starts again after penalty(house light off)
+# Task file for 5-CSRTT task which can be used for training stages 1-5, and all challanges, by adjusting task variables as described below. 
 
+# One of the five_poke LEDs turns on randomly (cue presentation) and subject has to make response within the specified period of time(SD+LH)
+# cue presentation ==> Correct poke (illuminated hole)                  ==> reward(@reward port)      ==> iti** ==> cue presentation
+# cue presentation ==> Incorrect response (other than illuminated hole) ==> penalty (house light off) ==> iti** ==> cue presentation
+# cue presentation ==> omission (no response)                           ==> penalty (house light off) ==> iti** ==> cue presentation
+ 
 # Important readouts:
 # Responses :- number of Corrects, number of Incorrects, number of Omissions, number of Prematures
 # Latencies :-Reward lat., Premature lat., Correct lat.,
 
-# iti duration randomnly picked by programme from iti list
-# important note:- if reward port SOL not used and intended to use 'house_light = Digital_output(pin=board.port_5.POW_A)'
-#  ....then this line of code 'hw.reward_port.SOL.on()'  need to modify as 'hw.house_light.on()' to control HouseLight 
+# If Subject made criteria for two consecutive days, then task can be advanced to stage-1 to satge-2 and so on upto stage-5.
+# Detailed description and criteria list is provided in the manuscript 
+
+# VARIABLES CHANGED ACROSS TRAINING STAGES AND CHALLANGES
+
+# Stage 1: v.state_dur = 20 *second , v.ITI_dur = 2 *second
+# Stage 2: v.state_dur = 8 *second  , v.ITI_dur = 2 *second
+# Stage 3: v.state_dur = 8 *second  , v.ITI_dur = 5 *second
+# Stage 4  v.state_dur = 4 *second  , v.ITI_dur = 5 *second
+# Stage 5: v.state_dur = 2 *second  , v.ITI_dur = 5 *second
+
+# Attention Challenge 1000ms : v.state_dur = 1 *second    , v.ITI_dur = 5 *second
+# Attention Challenge 800ms  : v.state_dur = 0.8 *second  , v.ITI_dur = 5 *second
+# FixedITI Challenge         : v.state_dur = 2 *second    , v.ITI_dur = 9 *second
+# VariableITI challange      : v.variable_ITI = True
+# Sound distraction challange: v.sound_distraction = True
 
 #--------------------------------------------------------------------------
 
-
-# necessary imports to begin code
+# Imports
 
 from pyControl.utility import *
 import hardware_definition as hw
 import random
 
-
-# list of states and events
+# States and events
 
 states = ['start',
           'choice_task', #Cue Light presentation
@@ -41,18 +54,22 @@ events = ['session_timer',      # timer to terminate session after defined time 
           'poke_6_out',         # reward receptacle out
           'port_lights_timer',  # turn off five choice light and advance to limited hold(LH) stage
           'reward_in_timer',    # turn off reward port light to reinforce to leave receptacle
-          'penalty_omission']   # timer to wait for response for defined period
+          'penalty_omission',   # timer to wait for response for defined period
+          'distraction_on_timer',  # timer to turn distractor sound on
+          'distraction_off_timer'] # timer to turn distractorsound off
 
-# initial state name (required by pyControl)
 initial_state = 'start'
 
-# variables
-# variables changes according to stages
-iti_list    = [7, 9, 11, 13] # iti list values represents in sec, can be changed accornding to experiment
-v.state_dur = 20 * second    # cue stimulus duration
+# Variables
 
+# Paramters adjusted for different training stages and challanges.
 
-#constant variable for all stages
+v.state_dur = 20 * second      # cue stimulus duration
+v.ITI_dur   = 2 * second       # inter trial interval duration
+v.variable_ITI = False         # whether to use variable ITI.
+v.sound_distraction = False    # whether to use sound distraction.
+
+# Other parameters.
 
 v.session_dur   = 30 * minute  # Session duration
 v.LH_dur        = 2 * second   # limited hold duration
@@ -61,23 +78,22 @@ v.penalty_dur   = 5 * second   # penalty duration(All lights off)
 v.steps_rate    = 1000         # step rate for stepper motor if you want to use peristaltic pump
 v.n_steps       = 1300         # number of steps for stepper motor if you want to use peristaltic pump
 v.target        = 0
-v.ITI_dur       = 0            # this duration randomly select from the above list
 
+# Run start and end behaviour.
 
-# states defines starts and ending of task file
 def run_start():
     set_timer('session_timer', v.session_dur)
-    hw.reward_port.SOL.on() # turn on house light - we use reward port SOL to power house light
+    hw.house_light.on() # turn on house light - we use reward port SOL to power house light
 
 
 def run_end():
     hw.off() # Turn off the Hardware
-    hw.reward_port.SOL.off() # turn off house light - we use reward port SOL to power house light
+    hw.house_light.off() # turn off house light - we use reward port SOL to power house light
 
+# State behaviour functions.
 
-# initial state
-#5-CSRTT always starts by delivering reward
 def start(event):
+	# 5-CSRTT always starts by delivering reward
     if event == 'entry':
         hw.reward_port.LED.on()                           # turn on reward port LED on
         hw.syringe_pump.backward(v.steps_rate, v.n_steps) # beginning reward
@@ -91,8 +107,8 @@ def start(event):
         goto_state('choice_task')
 
 
-# task phase which present random cue
 def choice_task(event):
+	# task phase which present random cue
     if event == 'entry':
         print('Sample_state')
         set_timer('port_lights_timer', v.state_dur)
@@ -137,8 +153,8 @@ def choice_task(event):
         disarm_timer('penalty_omission')  # clear timer for omission
 
 
-# reward state
 def reward(event):
+	# reward state
     if event == 'entry':
         hw.reward_port.LED.on()
     elif event == 'exit':
@@ -157,16 +173,15 @@ def reward(event):
             or event == 'poke_5' and v.target == 5:
         print('perseverate')
 
-
-# penalty state
 def penalty(event):
+	# penalty state
     if event == 'entry':
         print('penalty')
-        hw.reward_port.SOL.off()  # house light off
+        hw.house_light.off()  # house light off
         hw.reward_port.LED.off()  # reward port light off
         timed_goto_state('iti', v.penalty_dur)
     elif event == 'exit':
-        hw.reward_port.SOL.on()
+        hw.house_light.on()
     elif event == 'poke_1' \
             or event == 'poke_2' \
             or event == 'poke_3' \
@@ -174,25 +189,32 @@ def penalty(event):
             or event == 'poke_5':
         print('attempts_dur_penalty')
 
-
-# iti state
 def iti(event):
+	# iti state
     if event == 'entry':
-        x = random.choice(iti_list)
-        v.ITI_dur = x*second
-        print('iti_dur:{}'.format(v.ITI_dur))
         print('iti_start_time')
-        timed_goto_state('choice_task', v.ITI_dur)
+        if v.variable_ITI: # Use variable ITI
+            rand_ITI_dur = random.choice([7, 9, 11, 13])*second
+            print('iti_dur:{}'.format(rand_ITI_dur))
+            timed_goto_state('choice_task', rand_ITI_dur)
+        else: # Use fixed ITI.
+            timed_goto_state('choice_task', v.ITI_dur)
+        if v.sound_distraction: # Set timer to trigger distracting stimuli.
+            sound_start_delay = random.randint(500, 3500)*ms
+            set_timer('distraction_on_timer',  sound_start_delay)        # when to start the sound 
+            set_timer('distraction_off_timer', sound_start_delay + 1000) # when to stop the sound 
+
     elif event == 'poke_1' \
             or event == 'poke_2' \
             or event == 'poke_3' \
             or event == 'poke_4' \
             or event == 'poke_5':
-        print('Premature_response')
+        print('Premature_response') # the latency between iti_start_time and Premature_response is premature lat.
         goto_state('penalty')
 
 
-# state indipendent behavior
+# State indipendent behavior
+
 def all_states(event):
     # When 'session_timer' event occurs stop framework to end session.
     if event == 'session_timer':
@@ -214,4 +236,9 @@ def all_states(event):
         print('5_poke_entries')
     elif event == 'poke_6_out':
         print('receptacle_entries')
-
+    elif event == 'distraction_on_timer':
+        hw.speaker.set_volume(35)
+        hw.speaker.noise()
+    elif event == 'distraction_off_timer':
+        hw.speaker.set_volume(5)
+        hw.speaker.off()
